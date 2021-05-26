f8f4259d64ac526d5e68aaaf867066d24ba2efb9
SoftechMansoor <softech1900@gmail.com>
2021-05-27 00:12:07 +0500

    Add files via upload
    Website Contact Us Form Using PHP. It will send email to your email address. It is ideal to use on a real hosting. May show error if run on Local Host.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact Us</title>
+</head>
+<body>
+<main>
+	<p>send E-mail</p>
+	<form class="#" action="contactform.php" method="POST">
+<input type="text" name="name" placeholder="Full name">
+<input type="text" name="mail" placeholder="E-mail">
+<input type="text" name="subject" placeholder="Subject">
+<textarea  name="message"></textarea>
+
+<button type="submit" name="submit">SEND MAIL</button>
+	</form>
+</main>
+</body>
+</html>
